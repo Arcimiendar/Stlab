aacdfe0e3970e9f05ac9d0476123f3da5b6df1e9
artimendor <artimendor@yandex.ru>
2019-02-13 20:49:50 +0300

    done some code review advices!

--- main/dz3/zadacza2.py
+++ main/dz3/zadacza2.py
@@ -9,20 +9,20 @@ File = typing.TextIO
 
 def merge_students_data(csv_file: File, xlsx_workbook, json_file: File) \
         -> typing.NoReturn:
-    wb = xlsx_workbook
+    workbook = xlsx_workbook
     students_csv = csv.DictReader(csv_file)
-    sheet = wb[wb.sheetnames[0]]
+    sheet = workbook[workbook.sheetnames[0]]
 
-    res_dict = dict()
+    further_json = dict()
 
-    for name_row in range(len(sheet['A'])):
-        res_dict[sheet[f'A{name_row + 1}'].value] = \
-            {'marks': [mark.value for mark in sheet[str(name_row + 1)] if
+    for number_of_row in range(len(sheet['A'])):
+        further_json[sheet[f'A{number_of_row + 1}'].value] = \
+            {'marks': [mark.value for mark in sheet[str(number_of_row + 1)] if
                        isinstance(mark.value, int) and not isinstance(mark.value, bool)]}  # if value is int
 
     for student in students_csv:
         name = student['first_name'] + ' ' + student['last_name']
 
-        res_dict[name]['age'] = int(student['age'])
+        further_json[name]['age'] = int(student['age'])
 
-    json.dump(res_dict, json_file)
+    json.dump(further_json, json_file)
