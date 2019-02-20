f246b8abab53ebe5713558f85dbb396a48899b29
artimendor <artimendor@yandex.ru>
2019-02-20 21:16:54 +0300

    added get weather

--- main/dz5/HTTPtask.py
+++ main/dz5/HTTPtask.py
@@ -1,5 +1,5 @@
 from requests import get
-from json import dumps
+import re
 
 
 def exchange_rates(currency_abbreviation: str) -> float:
@@ -17,8 +17,29 @@ def exchange_rates(currency_abbreviation: str) -> float:
 
 def get_weather():
 
+    pattern_page = r"<a class=\"link link_theme_normal " \
+              r"place-list__item-name i-bem\" tabindex=\"0\" href=\"" \
+              r"(?P<ref>/pogoda/[a-z-]*)\" (?:[\w-]*)='{\"link\":{}}'>" \
+              r"(?P<name>[А-ЯЁ][ёа-я]+(?:[ -][ЁА-Я][ёа-я]+)?)</a>"
+
+    pattern_temperature = r"(?<=<div class=\"temp fact__temp fact__temp_size_s\"><span class=\"temp_" \
+                          r"_value\">)(?:\+)?(?P<temprt>[−]?[0-9]+)(?=</span><span class=\"temp__unit" \
+                          r" i-font i-font_face_yandex-sans-text-medium\">°</span></div>)"
     data = get("https://yandex.by/pogoda/region/149")
+    cities = re.findall(pattern_page, data.text)
 
-    print(data.text)
+    weather_dict = dict()
 
+    for city in cities:
+        url = "https://yandex.by" + city[0]
 
+        data = get(url)
+
+        value = re.search(pattern_temperature, data.text)[0]
+
+        if value[0] == chr(8722):
+            value = -1 * int(value[1:])
+
+        weather_dict[city[1]] = int(value)
+
+    return weather_dict
